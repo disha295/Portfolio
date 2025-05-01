a0796506871f9a3062e596e10d3b8a93a8b9368d
Disha <dvshetiya@gmail.com>
2025-04-30 22:24:23 -0500

    fix: set basename for BrowserRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/disha295.github.io-Portfolio/">
       <App />
     </BrowserRouter>
   </StrictMode>
