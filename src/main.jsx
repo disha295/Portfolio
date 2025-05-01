abb17aa1b055bdf1e841ff5344a3b40f357fdb6a
Disha <dvshetiya@gmail.com>
2025-04-30 23:26:08 -0500

    fix: remove basename from BrowserRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/disha295.github.io-Portfolio/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
